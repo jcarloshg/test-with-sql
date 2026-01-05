415f8aaa70490d2ef204a906cf4ce80dde501764
jcarloshg <carlosj12336@gmail.com>
2026-01-05 14:46:23 -0600

    add result for sql tests

--- test/02.inter_view/test/04.common-table-expressions.sql
+++ test/02.inter_view/test/04.common-table-expressions.sql
@@ -41,4 +41,20 @@ FROM
     JOIN DepartmentBenchmarks b ON e.department = b.department
 WHERE
     e.salary > b.avg_dept_salary
-ORDER BY e.department, e.salary DESC;
+ORDER BY e.id DESC;
+
+-- RESULT:
+-- +-----+----------------+------------------+--------+--------------------+------------------+
+-- | id  | name           | department       | salary | department_average | amount_above_avg |
+-- +-----+----------------+------------------+--------+--------------------+------------------+
+-- | 998 | Alice Williams | Product          | 175337 | 117574.54          | 57762.46         |
+-- | 996 | Emma Smith     | Customer Support | 127355 | 118577.41          | 8777.59          |
+-- | 995 | David Davis    | Sales            | 133736 | 116888.56          | 16847.44         |
+-- | 994 | Charlie Brown  | Marketing        | 161178 | 113511.29          | 47666.71         |
+-- | 993 | Bob Brown      | Sales            | 133574 | 116888.56          | 16685.44         |
+-- | 990 | Noah Martinez  | Sales            | 144882 | 116888.56          | 27993.44         |
+-- | 988 | Liam Johnson   | Marketing        | 136724 | 113511.29          | 23212.71         |
+-- | 987 | Eve Smith      | Product          | 158522 | 117574.54          | 40947.46         |
+-- | 983 | Olivia Jones   | Product          | 166586 | 117574.54          | 49011.46         |
+-- | 982 | Emma Jones     | Marketing        | 128379 | 113511.29          | 14867.71         |
+-- +-----+----------------+------------------+--------+--------------------+------------------+
